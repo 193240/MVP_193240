8cd0c6481abb89292fa91965957abc9ed29f7729
SR <193240@ids.upchiapas.edu.mx>
2022-03-24 16:45:04 -0600

    base app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:mvp/screens/inicio_page.dart';
-
+import 'package:mvp/src/pages/inicio_page.dart';
+import 'package:mvp/src/pages/home_headers.dart';
+import 'package:mvp/src/splash/splash_view.dart';
+import 'package:mvp/src/Painters/progressView.dart';
+import 'package:mvp/src/pages/login_page.dart';
+import 'package:mvp/src/pages/registro.dart';
+import 'package:mvp/src/pages/recuperarPass.dart';
+import 'package:mvp/src/pages/mascotas.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -14,7 +20,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Inicio',
-      home: inicio_page()
+      initialRoute: 'initialHeader',
+      
+      routes: {
+        //'initialHeader': (BuildContext context)=> SplashView(),
+        'inicioHome': (BuildContext context)=> inicio_page(),
+        'practica': (BuildContext context)=>const homeHeader(),
+        'progressPainter': (BuildContext context)=> progressView(),
+        'login': (BuildContext context)=> login(),
+        'registro': (BuildContext context)=> Registro(),     
+        'recuperarPass': (BuildContext context)=> recuperarPass(),
+
+        'initialHeader': (BuildContext context)=> mascotasC(),
+      },
+      //home: inicio_page()
     );
   }
 }
